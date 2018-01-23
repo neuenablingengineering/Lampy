a73b503e9446a551bc634e962a1849928df5e3b2
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-01-23 13:43:29 -0500

    Updating set_manual_time to set the time

--- centrallamp/timekeeping/set_manual_time.sh
+++ centrallamp/timekeeping/set_manual_time.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# manual inputs should be either 'm' (to set time/date from prompts)
+# or arg1 == 'mm/dd/yyyy' arg2 == 'hh:mm' (in 24 hr time)
+
 # stop NTP services
-#pkill ntpd
+pkill ntpd
 
 # still need to modify this to set the date/time with  "date -s"
 # and receive the new time/date arguments from user input
@@ -12,15 +15,20 @@ then
 	read -p 'Minute: ' minute
 	read -p 'Numerical Month: ' month
 	read -p 'Numerical Day: ' day
+	read -p 'Year: ' year
 
 	echo "You entered $month/$day/2018 $hour:$minute"
 
 MANUALDATE="$month/$day/2018 $hour:$minute"
 else
-MANUALDATE="02/03/2017 18:13"
+MANUALDATE="$1 $2"
 
 fi
 echo $MANUALDATE
 
+date -s "$MANUALDATE"
+
+date
+
 # restart NTP services
-#/usr/sbin/ntpd
+/usr/sbin/ntpd
