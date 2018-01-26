9d8e2ae4f42484ae59485eb0289e4541749fecf5
Emily D <emdaniel@ccs.neu.edu>
2018-01-26 16:20:28 -0500

    Added .gitignore, added methods for display

--- centrallamp/outputs/lcd/display_message.py
+++ centrallamp/outputs/lcd/display_message.py
@@ -1,10 +1,17 @@
-import lcdlib
-import serial
+from lcdlib import LCDCommonCommands
 
-class display:
+import serial
+from time import strftime
+from datetime import datetime
+
+class LCDDisplay:
     BAUD_RATE = 9600
 
     def __init__(self):
-        self.ser = serial.Serial('/dev/serial0', BAUD_RATE)
+        self.ser = serial.Serial('/dev/serial0', self.BAUD_RATE)
 
-        
+    def write_msg_to_screen(self, msg):
+        self.ser.write(msg)
+
+    def write_time_to_screen(self):
+        self.ser.write(datetime.now().strftime('%H:%S'))
