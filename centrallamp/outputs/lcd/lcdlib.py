4bc6eba1c6cee0f9517304f05c2b3cde0833a6d0
Yasmeen <yemampoor@ccs.neu.edu>
2018-01-26 16:12:08 -0500

    made class

--- centrallamp/outputs/lcd/lcdlib.py
+++ centrallamp/outputs/lcd/lcdlib.py
@@ -1,49 +1,50 @@
+class LCDCommonCommands:
 #common commands
-def CLEAR(ser):
-    ser.write(chr(0xFE)) 
-    ser.write(chr(0x51))
+    def CLEAR(self, ser):
+        ser.write(chr(0xFE)) 
+        ser.write(chr(0x51))
 
-def DISPLAY_ON(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x41))
+    def DISPLAY_ON(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x41))
 
-def DISPLAY_OFF(ser):
-    ser.write(chr(0XFE))
-    ser.write(chr(0x42))
+    def DISPLAY_OFF(self, ser):
+        ser.write(chr(0XFE))
+        ser.write(chr(0x42))
 
-def CURSOR_HOME(ser):
-    ser.write(chr(0XFE))
-    ser.write(chr(0x46))
+    def CURSOR_HOME(self, ser):
+        ser.write(chr(0XFE))
+        ser.write(chr(0x46))
 
-def CURSOR_UNDERLINE_ON(ser):
-    ser.write(chr(0XFE))
-    ser.write(chr(0x47))
+    def CURSOR_UNDERLINE_ON(self, ser):
+        ser.write(chr(0XFE))
+        ser.write(chr(0x47))
 
-def CURSOR_UNDERLINE_OFF(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x48))
+    def CURSOR_UNDERLINE_OFF(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x48))
 
-def CURSOR_LEFT(ser):
-   ser.write(chr(0XFE))
-    ser.write(chr(0x49))
+    def CURSOR_LEFT(self, ser):
+        ser.write(chr(0XFE))
+        ser.write(chr(0x49))
 
-def CURSOR_RIGHT(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x4A))
+    def CURSOR_RIGHT(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x4A))
 
-def CURSOR_BLINK_ON(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x4B))
+    def CURSOR_BLINK_ON(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x4B))
 
-def CURSOR_BLINK_OFF(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x4C))
+    def CURSOR_BLINK_OFF(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x4C))
 
-def BACKSPACE(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x4E))
+    def BACKSPACE(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x4E))
 
-def SCROLL_LEFT(ser):
-    ser.write(chr(0xFE))
-    ser.write(chr(0x55))
+    def SCROLL_LEFT(self, ser):
+        ser.write(chr(0xFE))
+        ser.write(chr(0x55))
 
