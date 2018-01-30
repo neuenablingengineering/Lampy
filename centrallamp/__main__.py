a4096040b6a474bcb91bc9c55c6db2412fbf6a78
emilydaniel <daniel.em@husky.neu.edu>
2018-01-30 17:15:35 -0500

    Clock, prompts input in terminal, prints input on LCD, back to clock

--- centrallamp/__main__.py
+++ centrallamp/__main__.py
@@ -1,31 +1,41 @@
 import threading
 import time
+import sys
 from outputs.lcd.display_message import LCDDisplay
+lcd = LCDDisplay()
+
+class TimeThread(threading.Thread):
+    def __init__(self, lcd):
+        threading.Thread.__init__(self)
+        lcd = lcd
+    def run(self):
+        while (True):
+            lcd.write_time_to_screen()
+            time.sleep(60)
+
+class InputThread(threading.Thread):
+    def __init__(self, lcd):
+        threading.Thread.__init__(self)
+        lcd = lcd
+    def run(self):
+        inp = raw_input("Give input:")
+        lcd.write_msg_to_screen(inp)
+        time.sleep(30)
+        sys.exit() 
 
 def main():
     lcd_lock = threading.Lock();
 
-    lcd = LCDDisplay()
+    
+    t_thread = TimeThread(lcd)
+    inp_thread = InputThread(lcd)
+    t_thread.start()
+    inp_thread.start()
 
-    getInput = threading.Thread(group=None
-        , target=get_in(lcd, lcd_lock))
-    displayTime = threading.Thread(group=None
-        , target=keep_time(lcd, lcd_lock))
-
-def keep_time(lcd, lcd_lock):
-    while (True):
- #       if (lcd_lock.locked() == False):
-        lcd.write_time_to_screen() 
-        time.sleep(60)
-
-def get_in(lcd, lcd_lock):
-    inp = raw_input('Input: ')
-#    lcd_lock.acquire()
-#    print('Lock acquired')
-    lcd.write_msg_to_screen(inp)
-    time.sleep(15)
-#    lcd_lock.release()
-#    print('Lock released')
+    
 
 if __name__ == "__main__":
     main()
+
+
+
