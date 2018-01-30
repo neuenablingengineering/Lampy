f7832827b7811f004de1494215173eaf1580b6a2
emilydaniel <daniel.em@husky.neu.edu>
2018-01-30 13:35:47 -0500

    readgpio

--- centrallamp/inputs/read_gpio.py
+++ centrallamp/inputs/read_gpio.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(29, GPIO.IN)
+GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while(True):
     print(GPIO.input(29))
