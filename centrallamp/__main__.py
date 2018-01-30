bb0a29ce3e780d0443d32db435de496b2a76523c
emilydaniel <daniel.em@husky.neu.edu>
2018-01-30 14:04:36 -0500

    lock not working. Asks for input first, prints input, then moves to clock. Does not work in reverse order

--- centrallamp/__main__.py
+++ centrallamp/__main__.py
@@ -2,29 +2,30 @@ import threading
 import time
 from outputs.lcd.display_message import LCDDisplay
 
-
 def main():
-    lcd_lock = thread.allocate_lock();
+    lcd_lock = threading.Lock();
+
     lcd = LCDDisplay()
-    displayTime = threading.Thread(group=None
-        , target=keepTime(lcd, lcd_lock)
-        , name=Time)
+
     getInput = threading.Thread(group=None
-        , target=getIn(lcd, lcd_lock)
-        , name=Input)
+        , target=getIn(lcd, lcd_lock))
+    displayTime = threading.Thread(group=None
+        , target=keepTime(lcd, lcd_lock))
 
 def keepTime(lcd, lcd_lock):
     while (True):
-        if (!lcd_lock.locked()):
-            lcd.write_time_to_screen() 
-            time.sleep(60)
+ #       if (lcd_lock.locked() == False):
+        lcd.write_time_to_screen() 
+        time.sleep(60)
 
 def getIn(lcd, lcd_lock):
-    inp = input("Input: ")
-    lcd_lock.aquire()
+    inp = raw_input('Input: ')
+#    lcd_lock.acquire()
+#    print('Lock acquired')
     lcd.write_msg_to_screen(inp)
-    time.sleep(30)
-    lcd_lock.release()
+    time.sleep(15)
+#    lcd_lock.release()
+#    print('Lock released')
 
 if __name__ == "__main__":
     main()
