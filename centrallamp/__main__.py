3a93d9e650d6ca1fba4243956235e2ce35327c1c
Yasmeen <yemampoor@ccs.neu.edu>
2018-01-31 13:02:58 -0500

    Added getInput for test

--- centrallamp/__main__.py
+++ centrallamp/__main__.py
@@ -6,12 +6,19 @@ from outputs.lcd.display_message import LCDDisplay
 def main():
     lcd = LCDDisplay()
     displayTime = threading.Thread(group=None, target=keepTime(lcd), name=Time)
+    getInput = threading.Thread(group=None, target=getIn(lcd),
+name=Input)
+
 
 def keepTime(lcd):
     while (True):
         lcd.write_time_to_screen() 
         time.sleep(60)
-        
+
+def getIn(lcd):
+    in = input("Input: ")
+    lcd.write_msg_to_screen(in)
+    time.sleep(30)
 
 if __name__ == "__main__":
     main()
