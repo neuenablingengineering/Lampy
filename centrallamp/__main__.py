33683e3267122a6a2b635248232e4369125c04bb
Emily D <emdaniel@ccs.neu.edu>
2018-01-31 12:53:55 -0500

    Added main and init.py files

---
+++ centrallamp/__main__.py
@@ -0,0 +1,17 @@
+import threading
+import time
+from outputs.lcd.display_message import LCDDisplay
+
+
+def main():
+    lcd = LCDDisplay()
+    displayTime = threading.Thread(group=None, target=keepTime(lcd), name=Time)
+
+def keepTime(lcd):
+    while (true):
+        lcd.write_time_to_screen() 
+        time.sleep(60)
+        
+
+if __name__ == "__main__":
+    main()
