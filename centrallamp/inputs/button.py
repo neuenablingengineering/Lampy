e1e8b97cea2f96c9651df8a7498ed1ca6a3d5d22
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-02 12:33:26 -0500

    Testing a potential fix

--- centrallamp/inputs/button.py
+++ centrallamp/inputs/button.py
@@ -9,12 +9,15 @@ class Button:
 		GPIO.setup(self.pinNum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		# add event to detect rising and falling edges
 		GPIO.add_event_detect(self.pinNum,GPIO.BOTH)
-		GPIO.add_event_callback(self.pinNum, self.detectEdge())
+		GPIO.add_event_callback(self.pinNum, self.my_callback)
 
 	def getState(self):
 		# return whether pin is "ON" or "OFF"
 		return GPIO.input(self.pinNum)
 	
-	def detectEdge(self):
-		print "Button changed"
-		return True
+	#def detectEdge(self):
+	#	print "Button changed"
+	#	return True
+
+	def my_callback():
+		print "Button pressed"
