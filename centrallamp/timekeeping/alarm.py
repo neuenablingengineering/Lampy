4ed95176f0f9fd0d4363ccef30314b005ebc5441
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-02 13:40:47 -0500

    Added new print formatting and increment functions for minute/hour

--- centrallamp/timekeeping/alarm.py
+++ centrallamp/timekeeping/alarm.py
@@ -9,7 +9,9 @@ class Alarm:
 			self.hour = hour
 			self.minute = minute
 		else:
-			print("Error: Not in range")
+			print("Error: Not in range. Default 12:00 alarm set")
+			self.hour = 12
+			self.minute = 0
 	# setter -- should not allow setting outside of specified ranges
 	def setAlarm(self, hour, minute):
 		if hour in range(0,23) and minute in range(0,59):
@@ -19,7 +21,7 @@ class Alarm:
 			print("Error: Not in range")
 	# getter -- return string of alarm time
 	def getAlarm(self):
-		return("%s:%s" % (self.hour, self.minute))
+		return("%02d:%02d" % (self.hour, self.minute))
 	# compare alarm time to system time and return true or false
 	def checkTime(self):
 		now = datetime.datetime.now()
@@ -27,3 +29,12 @@ class Alarm:
 			return True
 		else:
 			return False
+	# increment the stored hour, don't allow hour outside of 
+	def incHour(self):
+		self.hour += 1
+		if self.hour >23:
+			self.hour = 0
+	def incMin(self):
+		self.minute += 1
+		if self.minute > 59:
+			self.minute = 0
