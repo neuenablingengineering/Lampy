1d9a563fe5314c93db75c4d85e592da4b3b7aafc
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-02 13:01:22 -0500

    debouncing okay, now trying to figure out callbacks

--- centrallamp/inputs/button.py
+++ centrallamp/inputs/button.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 
-def my_callback(self):
-	print "Button pushed"
+
 
 class Button:
 
@@ -11,8 +10,9 @@ class Button:
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(self.pinNum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		# add event to detect rising and falling edges
-		GPIO.add_event_detect(self.pinNum,GPIO.BOTH,bouncetime=300)
-		GPIO.add_event_callback(self.pinNum, my_callback)
+		# bouncetime is in MILLISECONDS
+		GPIO.add_event_detect(self.pinNum,GPIO.BOTH,bouncetime=1000)
+		GPIO.add_event_callback(self.pinNum, self.my_callback)
 
 	def getState(self):
 		# return whether pin is "ON" or "OFF"
@@ -21,3 +21,6 @@ class Button:
 	#def detectEdge(self):
 	#	print "Button changed"
 	#	return True
+
+	def my_callback(self):
+		print "Button pushed"
