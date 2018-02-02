5e8a62745736931c2759330283bdbee12c071993
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-02 13:23:14 -0500

    Forgetting mroe things

--- centrallamp/inputs/button_test.py
+++ centrallamp/inputs/button_test.py
@@ -4,17 +4,19 @@ import time
 
 testNum = 12
 
-testButton = Button(29)
-# add an event that listens for rising or falling edges on testButton
-# set up with a debounce time in MILLISECONDS
-GPIO.add_event_detect(testButton.getPin,GPIO.BOTH,bouncetime=1000)
-GPIO.add_event_callback(testButton.getPin, callback1)
-
 def callback1(self, testNum):
 	testNum += 1
 	if testNum > 23:
 		testNum = 0
 	print testNum
+
+testButton = Button(29)
+# add an event that listens for rising or falling edges on testButton
+# set up with a debounce time in MILLISECONDS
+GPIO.add_event_detect(testButton.getPin(),GPIO.BOTH,bouncetime=1000)
+GPIO.add_event_callback(testButton.getPin(), callback1)
+
+
 
 while(True):
 	print testButton.getState()
