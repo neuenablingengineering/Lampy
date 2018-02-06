5aa63f5a059a48a2a2917315001f0b64a3db744a
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-06 12:37:01 -0500

    Add minute button to demo

--- centrallamp/button_test.py
+++ centrallamp/button_test.py
@@ -7,16 +7,24 @@ import time
 testAlarm = Alarm(23,58)
 display = LCDDisplay()
 
-def callback1(self):
+def callbackHour(self):
 	global testAlarm
 	global display
 	testAlarm.inc_hour()
+def calbackMin(self):
+	global testAlarm
+	global display
+	testAlarm.inc_min()
 
-testButton = Button(29)
-# add an event that listens for rising or falling edges on testButton
+hourButton = Button(29)
+minButton = Button(31)
+# add events that listen for rising or falling edges on hourButton and minButton
 # set up with a debounce time in MILLISECONDS
-GPIO.add_event_detect(testButton.get_pin(),GPIO.BOTH,bouncetime=500)
-GPIO.add_event_callback(testButton.get_pin(), callback1)
+GPIO.add_event_detect(hourButton.get_pin(),GPIO.BOTH,bouncetime=500)
+GPIO.add_event_detect(minButton.get_pin(),GPIO.BOTH,bouncetime=500)
+
+GPIO.add_event_callback(hourButton.get_pin(), callbackHour)
+GPIO.add_event_callback(minButton.get_pin(), callbackMin)
 
 display.write_msg_to_screen("Button Demo")
 time.sleep(10)
