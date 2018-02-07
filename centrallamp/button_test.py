6274fcbde23d328b9775e247d3dd89c430ca5362
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-07 13:21:59 -0500

    Attempting to fix test

--- centrallamp/button_test.py
+++ centrallamp/button_test.py
@@ -9,16 +9,18 @@ display = LCDDisplay()
 
 def callback_hour(self):
     global testAlarm
-    global display
     global alarmToggle
     if(alarmToggle.get_state()):
         testAlarm.inc_hour()
 def callback_min(self):
     global testAlarm
-    global display
     global alarmToggle
     if(alarmToggle.get_state()):
         testAlarm.inc_min()
+def callback_toggle(self):
+    global display
+    display.write_msg_to_bottom_screen("Set Alarm Mode")
+    time.sleep(5)
 
 # define buttons and map them to GPIO pins
 alarmToggle = Button(11)
@@ -33,14 +35,15 @@ GPIO.add_event_detect(minButton.get_pin(),GPIO.BOTH,bouncetime=750)
 # add callbacks to buttons so something happens when the event happens
 GPIO.add_event_callback(hourButton.get_pin(), callback_hour)
 GPIO.add_event_callback(minButton.get_pin(), callback_min)
+GPIO.add_event_callback(alarmToggle.get_pin(), callback_toggle)
 
 display.write_msg_to_screen("Button Demo")
 time.sleep(3)
 while(True):
-    if(alarmToggle.get_state()):
-        display.write_msg_to_bottom_screen("Alarm Set Mode")
-        time.sleep(2)
-    else:
-        display.write_msg_to_bottom_screen("                ")
+#    if(alarmToggle.get_state()):
+#        display.write_msg_to_bottom_screen("Alarm Set Mode")
+#        time.sleep(2)
+#    else:
+#        display.write_msg_to_bottom_screen("                ")
     display.write_msg_to_screen("Alarm: %s" % testAlarm.get_alarm())
     time.sleep(0.5)    
