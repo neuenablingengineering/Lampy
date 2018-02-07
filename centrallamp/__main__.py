1d3ded3efdf65f5a86b058d1ec63a440da733e0e
Yasmeen <yemampoor@ccs.neu.edu>
2018-02-07 11:24:22 -0500

    global lock

--- centrallamp/__main__.py
+++ centrallamp/__main__.py
@@ -5,6 +5,7 @@ import datetime as dt
 from outputs.lcd.display_message import LCDDisplay
 
 lcd = LCDDisplay()
+lcd_lock = threading.Lock();
 
 class TimeThread(threading.Thread):
     def __init__(self):
@@ -14,7 +15,8 @@ class TimeThread(threading.Thread):
         currMin = timenow.minute
         lcd.write_time_to_screen()
         while (True):
-            if (currMin != dt.datetime.now().minute):
+            if ((currMin != dt.datetime.now().minute) 
+                && !lcd_lock.locked()):
                 lcd.write_time_to_screen()
                 currMin = dt.datetime.now().minute 
 
@@ -23,14 +25,18 @@ class InputThread(threading.Thread):
         threading.Thread.__init__(self)
     def run(self):
         inp = raw_input("Give input:")
+        lcd_lock.aquire()
         lcd.write_msg_to_screen(inp)
         time.sleep(20)
+        release_lock()
         sys.exit() 
 
-def main():
-    lcd_lock = threading.Lock();
+def release_lock():
+    print "Releasing the lock"
+    lcd_lock.release()
+    lcd.write_time_to_screen()
 
-    
+def main():
     t_thread = TimeThread()
     inp_thread = InputThread()
     t_thread.start()
