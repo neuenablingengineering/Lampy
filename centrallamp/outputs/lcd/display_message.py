f4e015adcc4d0a545c813bd6f5ba33611a0272c6
Yasmeen <yemampoor@ccs.neu.edu>
2018-02-07 16:51:08 -0500

    additional display functions

--- centrallamp/outputs/lcd/display_message.py
+++ centrallamp/outputs/lcd/display_message.py
@@ -17,6 +17,31 @@ class LCDDisplay:
             self.com.CLEAR(self.ser)
             self.ser.write(msg)
 
+    def write_two_line_msg(self, line1, line2):
+        if (self.ser.isOpen()):
+            self.com.CLEAR(self.ser)
+            self.ser.write(line1)
+            self.com.CURSOR_SET(self.ser, 16)
+            self.ser.write(line2)
+
+    def write_to_second_line(self, msg):
+        if (self.ser.isOpen()):
+            self.com.clear_second_line(self.ser)
+            self.com.CURSOR_SET(self.ser, 16)
+            self.ser.write(msg)
+    
+    def clear_first_line(self):
+        if (self.ser.isOpen()):
+            self.com.CURSOR_SET(self.ser, 15)
+            for x in range(0, 15):
+                self.com.BACKSPACE(self.ser)
+
+    def clear_second_line(self):
+        if (self.ser.isOpen()):
+            self.com.CURSOR_SET(self.ser, 31)
+            for x in range(0, 15):
+                self.com.BACKSPACE(self.ser)
+
     def write_time_to_screen(self):
         if (self.ser.isOpen()):
             self.com.CLEAR(self.ser)
