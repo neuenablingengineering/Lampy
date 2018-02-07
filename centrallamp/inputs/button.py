33b213d62fd1ba37251081da2a7f7ea3d488cec0
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-07 12:57:54 -0500

    Replacing tabs

--- centrallamp/inputs/button.py
+++ centrallamp/inputs/button.py
@@ -2,14 +2,14 @@ import RPi.GPIO as GPIO
 
 class Button:
 
-	def __init__(self, pinNum):
-		# set GPIO mode and set selected pin for input
-		self.pinNum=pinNum
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(self.pinNum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    def __init__(self, pinNum):
+        # set GPIO mode and set selected pin for input
+        self.pinNum=pinNum
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pinNum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		
-	def get_state(self):
-		# return whether pin is "ON" or "OFF"
-		return GPIO.input(self.pinNum)
-	def get_pin(self):
-		return self.pinNum
+    def get_state(self):
+        # return whether pin is "ON" or "OFF"
+        return GPIO.input(self.pinNum)
+    def get_pin(self):
+        return self.pinNum
