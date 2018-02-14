fc966c3ae6b02500f5105dc3b3d496cb9b91e179
emilydaniel <daniel.em@husky.neu.edu>
2018-02-14 13:33:37 -0500

    still not working, added set_events function

--- centrallamp/inputs/set_alarm_mode.py
+++ centrallamp/inputs/set_alarm_mode.py
@@ -4,29 +4,29 @@ from outputs.lcd.display_message import LCDDisplay
 import RPi.GPIO as GPIO
 import time
 
+# define buttons and map them to GPIO pins
+hourButton = Button(29)
+minButton = Button(31)
+
 class SetAlarmMode:
     
     def __init__(self, dualAlarm):
         self.dualAlarm = dualAlarm
 
-    def callback_hour(self):
-        dualAlarm.inc_hour()
+    def callback_hour(self, channel):
+        self.dualAlarm.increment_both_hour()
     
-    def callback_min(self):
-        dualAlarm.inc_min()
-
-    def set_alarm_mode(self, 
-    # define buttons and map them to GPIO pins
-        hourButton = Button(29)
-        minButton = Button(31)
-
-    # add events that listen for rising or falling edges on hourButton and minButton
-    # set up with a debounce time in MILLISECONDS
-    GPIO.add_event_detect(hourButton.get_pin(),GPIO.BOTH,bouncetime=750)
-    GPIO.add_event_detect(minButton.get_pin(),GPIO.BOTH,bouncetime=750)
-
-    # add callbacks to buttons so something happens when the event happens
-    GPIO.add_event_callback(hourButton.get_pin(), callback_hour)
-    GPIO.add_event_callback(minButton.get_pin(), callback_min)
+    def callback_min(self,channel):
+        self.dualAlarm.increment_both_min()
 
 
+
+    def set_events(self):
+        # add events that listen for rising or falling edges on hourButton and minButton
+        # set up with a debounce time in MILLISECONDS
+        GPIO.add_event_detect(hourButton.get_pin(),GPIO.BOTH,bouncetime=750)
+        GPIO.add_event_detect(minButton.get_pin(),GPIO.BOTH,bouncetime=750)
+
+        # add callbacks to buttons so something happens when the event happens
+        GPIO.add_event_callback(hourButton.get_pin(), self.callback_hour)
+        GPIO.add_event_callback(minButton.get_pin(), self.callback_hour)
--- centrallamp/thread_classes.py
+++ centrallamp/thread_classes.py
@@ -2,8 +2,10 @@ import threading
 import time
 import sys
 import datetime as dt
+import RPi.GPIO as GPIO
 from inputs.button import Button
 from global_config import *
+from inputs.set_alarm_mode import SetAlarmMode
 
 class TimeThread(threading.Thread):
     def __init__(self):
@@ -11,15 +13,15 @@ class TimeThread(threading.Thread):
     def run(self):
         timenow = dt.datetime.now()
         currMin = timenow.minute
-        lcd.write_time_to_screen()
+        LCD.write_time_to_screen()
         while (True):
             if ((currMin != dt.datetime.now().minute)
-                &  (lcdControlBool == False)):
-                lcd.write_time_to_screen()
+                &  (LCD_CONTROL_BOOL == False)):
+                LCD.write_time_to_screen()
                 currMin = dt.datetime.now().minute
 
 class InputThread(threading.Thread):
-    alarmToggle = Button(11) 
+
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -28,15 +30,18 @@ class InputThread(threading.Thread):
         toggleFlag = True
 
     def run(self):
+        alarmToggle = Button(11) 
         GPIO.add_event_detect(alarmToggle.get_pin()
             , GPIO.RISING,bouncetime=750)
         GPIO.add_event_callback(alarmToggle.get_pin()
-            , callback_toggle)
+            , self.callback_toggle)
         while(True):
-            if(alarmToggle.get_state()):
+            if(alarmToggle):
                 LCD_CONTROL_BOOL = True
                 LCD.write_msg_to_screen("Alarm Set Mode")
                 setAlarmMode = SetAlarmMode(DAY_NIGHT_ALARM)
+                setAlarmMode.set_events()
+                alarmToggle = False
             time.sleep(5)
             LCD.write_msg_to_screen("Alarm: %s" %
                 DAY_NIGHT_ALARM.get_morning_alarm())
