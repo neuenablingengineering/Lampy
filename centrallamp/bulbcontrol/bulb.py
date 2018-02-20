0a1610835a15bd34fb9c059a1e92bfb24cdbb7df
Emily D <emdaniel@ccs.neu.edu>
2018-02-20 13:25:34 -0500

    fixed intentation, radability improvements

--- centrallamp/bulbcontrol/bulb.py
+++ centrallamp/bulbcontrol/bulb.py
@@ -5,24 +5,24 @@ import time
 
 class Bulb:
 
-	FREQENCY = 1000
-	SLEEP_DURING_TRANSITION_ON = 0.2
-	SLEEP_DURING_TRANSITION_OFF = 0.1
+    FREQENCY = 1000
+    SLEEP_DURING_TRANSITION_ON = 0.2
+    SLEEP_DURING_TRANSITION_OFF = 0.1
 
-	def __init__(self,pinNum,maxDutyCycle):
-		self.pinNum = pinNum
-		self.maxDutyCycle = maxDutyCycle
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(self.pinNum,GPIO.OUT)
-		self.pwm = GPIO.PWM(self.pinNum,FREQENCY)
-		self.pwm.start(0)
+    def __init__(self, pinNum, maxDutyCycle):
+        self.pinNum = pinNum
+        self.maxDutyCycle = maxDutyCycle
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pinNum,GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pinNum,FREQENCY)
+        self.pwm.start(0)
 
-	def transition_on(self):
-		for x in range(maxDutyCycle):
-    		self.pwm.ChangeDutyCycle(x)
-    		time.sleep(SLEEP_DURING_TRANSITION_ON)
+    def transition_on(self):
+        for x in range(self.maxDutyCycle):
+            self.pwm.ChangeDutyCycle(x)
+            time.sleep(SLEEP_DURING_TRANSITION_ON)
 
     def transition_off(self):
-    	for x in range(maxDutyCycle):
-    		self.pwm.ChangeDutyCycle(maxDutyCycle-x)
-    		time.sleep(SLEEP_DURING_TRANSITION_OFF)
+        for x in range(self.maxDutyCycle):
+            self.pwm.ChangeDutyCycle(self.maxDutyCycle - x)
+            time.sleep(SLEEP_DURING_TRANSITION_OFF)
