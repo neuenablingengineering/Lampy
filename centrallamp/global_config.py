03a56c32723298cc98b2086cadc09fc39e10c08f
emilydaniel <daniel.em@husky.neu.edu>
2018-02-24 18:38:38 -0500

    dual bulb container is in global config

--- centrallamp/global_config.py
+++ centrallamp/global_config.py
@@ -3,12 +3,11 @@ import datetime as dt
 from outputs.lcd.display_message import LCDDisplay
 from timekeeping.dual_alarm import DualAlarm
 from timekeeping.alarm import Alarm
-from bulbcontrol.bulb import Bulb
+from bulbcontrol.dual_bulb import DualBulb
 
 LCD = LCDDisplay()
 LCD_CONTROL_BOOL = False
 DAY_NIGHT_ALARM = DualAlarm()
 PANEL_STAY_AWAKE = Alarm()
-DAY_BULB = Bulb(12, 80, 0.1, 10)
-NIGHT_BULB = Bulb(13, 80, 10, 0.1)
+LAMP_BULBS = DualBulb(80)
 
