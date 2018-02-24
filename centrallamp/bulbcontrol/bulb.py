8dc3a772fc4353c842fcec8fb2c3ada53b52cf59
emilydaniel <daniel.em@husky.neu.edu>
2018-02-24 18:50:48 -0500

    different sleep durations for on and off can be set in constructor

--- centrallamp/bulbcontrol/bulb.py
+++ centrallamp/bulbcontrol/bulb.py
@@ -7,10 +7,15 @@ class Bulb:
 
     FREQUENCY = 1000
 
-    def __init__(self, pinNum, maxDutyCycle=80, transitionSleep=0.2):
+    def __init__(self
+        , pinNum
+        , maxDutyCycle=80
+        , transitionSleepOn=0.2
+        , transitionSleepOff=0.2):
         self.pinNum = pinNum
         self.maxDutyCycle = maxDutyCycle
-        self.transitionSleep = transitionSleep
+        self.transitionSleepOn = transitionSleepOn
+        self.transitionSleepOff = transitionSleepOff
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pinNum,GPIO.OUT)
         self.pwm = GPIO.PWM(self.pinNum,Bulb.FREQUENCY)
@@ -19,12 +24,12 @@ class Bulb:
     def transition_on(self):
         for x in range(self.maxDutyCycle):
             self.pwm.ChangeDutyCycle(x)
-            time.sleep(self.transitionSleep)
+            time.sleep(self.transitionSleepOn)
 
     def transition_off(self):
         for x in range(self.maxDutyCycle):
             self.pwm.ChangeDutyCycle(self.maxDutyCycle - x)
-            time.sleep(self.transitionSleep)
+            time.sleep(self.transitionSleepOff)
 
     def turn_off(self):
         self.pwm.stop()
