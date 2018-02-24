480071184f691324a221d4d1665e8bebc22fe897
emilydaniel <daniel.em@husky.neu.edu>
2018-02-24 18:23:18 -0500

    global Bulbs

--- centrallamp/global_config.py
+++ centrallamp/global_config.py
@@ -3,9 +3,12 @@ import datetime as dt
 from outputs.lcd.display_message import LCDDisplay
 from timekeeping.dual_alarm import DualAlarm
 from timekeeping.alarm import Alarm
+from bulbcontrol.bulb import Bulb
 
 LCD = LCDDisplay()
 LCD_CONTROL_BOOL = False
 DAY_NIGHT_ALARM = DualAlarm()
 PANEL_STAY_AWAKE = Alarm()
+DAY_BULB = Bulb(12, 80, 0.1, 10)
+NIGHT_BULB = Bulb(13, 80, 10, 0.1)
 
