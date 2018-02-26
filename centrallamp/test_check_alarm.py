ed6e1952e61fd857099b2d2b6f1188e3c1e045fc
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-26 14:33:04 -0500

    test check_alarms

---
+++ centrallamp/test_check_alarm.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+from global_config import *
+from time_display import *
+from set_alarm import *
+
+def main():
+    timeProc = Process(target = time_display)
+    setAlarmProc = Process(target = set_alarm_main)
+    checkAlarmProc = Process(target = check_alarms_main)
+    timeProc.start()
+    setAlarmProc.start()
+    checkAlarmProc.start()
+            
+    
+
+if __name__ == "__main__":
+    main()
+
