592c06c8bf6648dac484a66360682bc85fc950fa
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-02-26 14:47:05 -0500

    alarm sound and playing sound from check_alarm

--- centrallamp/check_alarms.py
+++ centrallamp/check_alarms.py
@@ -1,11 +1,18 @@
 from global_config import *
 import time
+from pygame import mixer
 
 def check_alarms_main():
+    mixer.init()
+    # Alarm sound courtesy FoolBoyMedia (c) Creative Commons Attribution Non-Commercial 3.0 License
+    # https://freesound.org/people/FoolBoyMedia/sounds/246390/
+    # https://creativecommons.org/licenses/by-nc/3.0/
+    mixer.music.load('outputs/sound/chiming-out_foolboymedia.mp3')
     while(True):
         if DAY_NIGHT_ALARM.check_morning_alarm():
             LAMP_BULBS.morning_sequence()
             # TODO placeholder for sound
+            mixer.music.play()
             # TODO placeholder for BLE communication with mat
             # TODO placeholder for BLE communication with panel
         if DAY_NIGHT_ALARM.check_dusk_sim_alarm():
