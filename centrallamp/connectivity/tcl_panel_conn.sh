8f167e740fdb7f3b794043514a352f6181c776ff
emilydaniel <daniel.em@husky.neu.edu>
2018-03-13 22:53:16 -0400

    Script to connect to panel and send M

---
+++ centrallamp/connectivity/tcl_panel_conn.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/expect -f
+
+set prompt "#"
+
+spawn bluetoothctl
+expect $prompt
+
+#send -- "power on"
+#expect "Agent registered"
+#expect $prompt
+
+#send -- "agent on"
+#expect "Agent registered"
+#expect $prompt
+
+send -- "trust C0:0F:3E:A9:48:2C\r"
+expect "trust succeeded"
+expect $prompt
+
+send -- "connect C0:0F:3E:A9:48:2C\r"
+expect "Connection successful"
+expect $prompt
+
+send -- "menu gatt\r"
+expect "Menu gatt"
+expect $prompt
+
+send -- "select-attribute /org/bluez/hci0/dev_C0_0F_3E_A9_48_2C/service0009/char000a\r"
+expect $prompt
+
+send -- "write 0x4D\r"
+expect $prompt
+
+send -- "back\r"
+expect $prompt
+
+send -- "disconnect C0:0F:3E:A9:48:2C\r"
+expect "eof"
