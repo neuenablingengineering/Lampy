6d834eb3be1935217b62f8f2f444a7518a86c039
emilydaniel <daniel.em@husky.neu.edu>
2018-03-25 14:34:01 -0400

    shutdown thread

--- centrallamp/__main__.py
+++ centrallamp/__main__.py
@@ -1,14 +1,19 @@
-from multiprocessing import Process
 from global_config import *
 from time_display import *
 from set_alarm import *
+from check_alarms import TriggerThread
+from multiprocessing import Process
+from shutdown import ShutdownThread
 
 def main():
     timeProc = Process(target = time_display)
-    setAlarmProc = Process(target = set_alarm_main)
     timeProc.start()
-    setAlarmProc.start()
-            
+    setAlarmThread = SetAlarmThread()
+    setAlarmThread.start()
+    triggerThread = TriggerThread()
+    triggerThread.start()
+    shutdownThread = ShutdownThread()
+    shutdownThread.start()            
     
 
 if __name__ == "__main__":
---
+++ centrallamp/shutdown.py
@@ -0,0 +1,24 @@
+from global_config import *
+import RPi.GPIO as GPIO
+import threading
+from inputs.button import Button
+import time
+import os
+
+def shutdown(channel):
+    print("Shutdown should be called")
+    LCD-write_msg_to_screen("  ")
+    GPIO.cleanup()
+    os.system("shutdown now -h")
+
+class ShutdownThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        #shutdownButton = Button() ### Need to add GPIO pin for shutdown button
+
+        GPIO.add_event_detect(shutdownButton.get_pin()
+            , GPIO.BOTH
+            , callback = shutdown
+            , bouncetime = 750)
