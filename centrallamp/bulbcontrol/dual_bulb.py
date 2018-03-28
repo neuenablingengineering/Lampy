62bb0a1db68da98e2235b72dd052fef0e1433a64
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-03-28 12:39:52 -0400

    only start with the day bulb turned on

--- centrallamp/bulbcontrol/dual_bulb.py
+++ centrallamp/bulbcontrol/dual_bulb.py
@@ -7,6 +7,8 @@ class DualBulb:
         self.maxDutyCycle = maxDutyCycle
         self.dayBulb = Bulb(12, 80, 0.1, 10)
         self.nightBulb = Bulb(13, 80, 10, 0.1)
+        self.dayBulb.changeDutyCycle(80)
+        self.nightBulb.changeDutyCycle(0)
 
     def morning_sequence(self):
         self.set_bulbs_on(0,0)
