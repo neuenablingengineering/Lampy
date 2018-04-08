b2b4daeb2bc8397bbcc04867e41b2576f3e2a028
Elizabeth Kairys <kairys.e@husky.neu.edu>
2018-04-08 09:59:08 -0400

    commented out panel afternoon alarm so it does not trigger during demo

--- centrallamp/check_alarms.py
+++ centrallamp/check_alarms.py
@@ -46,30 +46,28 @@ class TriggerThread(threading.Thread):
                             print "Couldn't find Matty in range"
                             time.sleep(1)
 
-
                     if mat_detected == 1:
                         subprocess.Popen(['expect', 'connectivity/tcl_panel_conn.sh', '0x4E'])
                         print "attempt to kill subproc"
                         subprocess.call(["pkill", "omx"])
                         self.isTriggeredMorning = False
 
-                        
                 if (DAY_NIGHT_ALARM.check_dusk_sim_alarm() and not self.isTriggeredNight):
                     self.isTriggeredNight = True
                     print "Evening alarm triggered"
                     LAMP_BULBS.demo_evening()
                 if not DAY_NIGHT_ALARM.check_dusk_sim_alarm():
                     self.isTriggeredNight = False
-                if (PANEL_STAY_AWAKE.check_time() and not self.isTriggeredStayAwake):
-                    self.isTriggeredStayAwake = True
-                    print "LED Panel alarm triggered"
-                    
-                    #BLE communication with panel -- light on
-                    subprocess.Popen(['expect', 'connectivity/tcl_panel_conn.sh', '0x4D'])
-                    
-                    time.sleep(1)
-                if not PANEL_STAY_AWAKE.check_time():
-                    self.isTriggeredStayAwake = False
+# Comment out panel-only alarm so it does not go off during demo
+#                if (PANEL_STAY_AWAKE.check_time() and not self.isTriggeredStayAwake):
+#                    self.isTriggeredStayAwake = True
+#                    print "LED Panel alarm triggered"
+#                    #BLE communication with panel -- light on
+#                    subprocess.Popen(['expect', 'connectivity/tcl_panel_conn.sh', '0x4D'])
+#                    time.sleep(1)
+#                if not PANEL_STAY_AWAKE.check_time():
+#                    self.isTriggeredStayAwake = False
+
                 # sleep for a while
                 print "Sleeping for five seconds..."
                 DAY_NIGHT_ALARM.print_both()
