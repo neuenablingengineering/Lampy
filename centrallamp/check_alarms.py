23611609a9d4be024a7ab63fa266bc6890a6dad3
emilydaniel <daniel.em@husky.neu.edu>
2018-04-08 08:51:55 -0400

    fix sleeps & trigger transition

--- centrallamp/check_alarms.py
+++ centrallamp/check_alarms.py
@@ -11,6 +11,7 @@ class TriggerThread(threading.Thread):
         self.isTriggeredMorning = False
         self.isTriggeredNight = False
         self.isTriggeredStayAwake = False
+        self.mat_detected = 0
 
     def run(self):
         # Alarm sound courtesy FoolBoyMedia (c) Creative Commons Attribution Non-Commercial 3.0 License
@@ -19,7 +20,8 @@ class TriggerThread(threading.Thread):
         while(True):
             if not DAY_NIGHT_ALARM.get_alarm_mode():
                 if (DAY_NIGHT_ALARM.check_morning_alarm() and not self.isTriggeredMorning):
-                    isTriggeredMorning = True
+                    self.isTriggeredMorning = True
+                    self.isTriggeredEvening = False
                      
                     print "Morning alarm triggered"
 
@@ -35,8 +37,8 @@ class TriggerThread(threading.Thread):
                     soundSubProc = subprocess.Popen(['omxplayer','--no-keys'
                         , '--amp', '1000', '--loop', 'outputs/sound/chiming-out_foolboymedia.mp3'])
 
-                    mat_detected = 0
-                    while mat_detected != 1:
+                    self.mat_detected = 0
+                    while self.mat_detected != 1:
                         #call the command and write to scan.txt file and then fill the process.
                         #loop to find if the MAC address given is available
                         os.system("hcitool scan> scan.txt & pkill --signal SIGINT hcitool")
@@ -44,15 +46,16 @@ class TriggerThread(threading.Thread):
                         readscan = scan.read()
                         if MATTY_ADDR in readscan:
                             print "Matty detected!"
-                            mat_detected = 1
+                            self.mat_detected = 1
                         else:
                             print "Couldn't find Matty in range"
                             time.sleep(1)
 
-                    if mat_detected == 1:
+                    if self.mat_detected == 1:
                         subprocess.Popen(['expect', 'connectivity/tcl_panel_conn.sh', '0x4E'])
                         print "attempt to kill subproc"
                         subprocess.call(["pkill", "omx"])
+                if ((not DAY_NIGHT_ALARM.check_morning_alarm()) and (self.mat_detected == 1)):
                         self.isTriggeredMorning = False
 
                 if (DAY_NIGHT_ALARM.check_dusk_sim_alarm() and not self.isTriggeredNight):
